c14a9383ad7e64b676756f34d613103cdf03a883
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-21 13:23:53 +0200

    Agregar archivos de proyecto.

---
+++ ParkyAPI/Datos/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opciones) : base (opciones)
+        {
+
+        }
+
+        public DbSet<ParqueNacional> parquesNacionales { get; set; }
+    }
+}
---
+++ ParkyAPI/Datos/Migrations/20210421112050_AddParquesNacionalesABD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkyAPI.Migrations
+{
+    public partial class AddParquesNacionalesABD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "parquesNacionales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ComunidadAutonoma = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Creado = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Establecido = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_parquesNacionales", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "parquesNacionales");
+        }
+    }
+}
