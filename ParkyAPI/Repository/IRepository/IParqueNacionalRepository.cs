0f69604bf6ffa769b7dbbd9db705c021394f2da8
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-21 15:53:15 +0200

    Añadir IParqueNacionalRepository y ParqueNacionalRepository.

---
+++ ParkyAPI/Repository/IRepository/IParqueNacionalRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyAPI.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface IParqueNacionalRepository
+    {
+        ICollection<ParqueNacional> GetParqueNacionales();
+        ParqueNacional GetParqueNacional(int parqueNacionalID);
+        bool ParqueNacionalExiste(string nombre);
+        bool parqueNacionalExiste(int id);
+        bool CrearParqueNacional(ParqueNacional parqueNacional);
+        bool UpdateParqueNacional(ParqueNacional parqueNacional);
+        bool DeleteParqueNacional(ParqueNacional parqueNacional);
+        bool Guardar();
+    }
+}
---
+++ ParkyAPI/Repository/ParqueNacionalRepository.cs
@@ -0,0 +1,64 @@
+﻿using ParkyAPI.Datos;
+using ParkyAPI.Modelos;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class ParqueNacionalRepository : IParqueNacionalRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ParqueNacionalRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CrearParqueNacional(ParqueNacional parqueNacional)
+        {
+            _db.parquesNacionales.Add(parqueNacional);
+            return Guardar();
+        }
+
+        public bool DeleteParqueNacional(ParqueNacional parqueNacional)
+        {
+            _db.parquesNacionales.Remove(parqueNacional);
+            return Guardar();
+        }
+
+        public ParqueNacional GetParqueNacional(int parqueNacionalID)
+        {
+            return _db.parquesNacionales.FirstOrDefault(a => a.Id == parqueNacionalID);
+        }
+
+        public ICollection<ParqueNacional> GetParqueNacionales()
+        {
+            return _db.parquesNacionales.OrderBy(a => a.Nombre).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool ParqueNacionalExiste(string nombre)
+        {
+            bool value = _db.parquesNacionales.Any(a => a.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            return value;
+        }
+
+        public bool parqueNacionalExiste(int id)
+        {
+            return _db.parquesNacionales.Any(a => a.Id == id);
+        }
+
+        public bool UpdateParqueNacional(ParqueNacional parqueNacional)
+        {
+            _db.parquesNacionales.Update(parqueNacional);
+            return Guardar();
+        }
+    }
+}
