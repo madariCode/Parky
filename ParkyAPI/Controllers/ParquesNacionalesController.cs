99902d4d6c57a6090f8ffc9468f0e6bd88e3df13
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-22 08:15:04 +0200

    Añadir ParquesNacionalesController.

---
+++ ParkyAPI/Controllers/ParquesNacionalesController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParquesNacionalesController : ControllerBase
+    {
+        private IParqueNacionalRepository _pnRepository;
+
+        private readonly IMapper _mapper;
+
+        public ParquesNacionalesController(IParqueNacionalRepository pnRepo, IMapper mapper)
+        {
+            _pnRepository = pnRepo;
+            _mapper = mapper;
+        }
+    }
+}
