7055969cc5068d4a8863831b266fbca9b2b6f279
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-23 08:25:17 +0200

    Añadir SenderoDto.

---
+++ ParkyAPI/Modelos/Dtos/SenderoDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkyAPI.Modelos.SenderoDto;
+
+namespace ParkyAPI.Modelos.Dtos
+{
+    public class SenderoDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public double Distancia { get; set; }
+        public TipoDificultad Dificultad { get; set; }
+        [Required]
+        public int ParqueNacionalId { get; set; }
+        public ParqueNacional ParqueNacionalDto { get; set; }
+    }
+}
