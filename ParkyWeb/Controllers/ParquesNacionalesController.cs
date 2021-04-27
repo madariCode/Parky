cf4ebef1c78c8feab5ebd84e1ef6eff5eeef262a
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-27 16:36:47 +0200

    Vista ParqueNacional Upsert, acción Post.

--- ParkyWeb/Controllers/ParquesNacionalesController.cs
+++ ParkyWeb/Controllers/ParquesNacionalesController.cs
@@ -3,6 +3,7 @@ using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,20 +27,62 @@ namespace ParkyWeb.Controllers
         {
             ParqueNacional obj = new ParqueNacional();
 
-            if(id == null)
+            if (id == null)
             {
                 //Insertar/Crear
                 return View(obj);
             }
-            
+
             //Update
             obj = await _pnRepo.GetAsync(SD.ParquesNacionalesAPIPath, id.GetValueOrDefault());
 
-            if(obj == null)
+            if (obj == null)
             {
                 NotFound();
             }
             return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(ParqueNacional obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var ficheros = HttpContext.Request.Form.Files;
+                if (ficheros.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = ficheros[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                        obj.Imagen = p1;
+                    }
+                }
+                else
+                {
+                    var objDeBD = await _pnRepo.GetAsync(SD.ParquesNacionalesAPIPath, obj.Id);
+                    obj.Imagen = objDeBD.Imagen;
+                }
+                if (obj.Id == 0)
+                {
+                    await _pnRepo.CreateAsync(SD.ParquesNacionalesAPIPath, obj);
+                }
+                else
+                {
+                    await _pnRepo.UpdateAsync(SD.ParquesNacionalesAPIPath + obj.Id, obj);
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
         }
 
         public async Task<IActionResult> GetAllParquesNacionales()
