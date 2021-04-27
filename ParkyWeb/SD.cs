eed5c377c702c09b28270c5b49e74f70be54e5f8
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-27 12:06:24 +0200

    Añadir SD.

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIUrlBase = "https://localhost:44345/";
+        public static string ParquesNacionalesAPIPath = APIUrlBase + "api/v1/parquesnacionales";
+        public static string SenderosAPIPath = APIUrlBase + "api/v1/senderos";
+    }
+}
