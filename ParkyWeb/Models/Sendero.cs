8dbed6bed8c0a89721c86ee6622b8b28e627a538
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-27 13:26:41 +0200

    Añadir modelos ParqueNacional y Sendero.

---
+++ ParkyWeb/Models/Sendero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Models
+{
+    public class Sendero
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public double Distancia { get; set; }
+        public enum TipoDificultad { Facil, Moderado, Dificil, Experto }
+        public TipoDificultad Dificultad { get; set; }
+        [Required]
+        public int ParqueNacionalId { get; set; }
+        public ParqueNacional ParqueNacionalDto { get; set; }
+    }
+}
