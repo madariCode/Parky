77bffea99b3eeaf95539642615dc8715bf40a458
Asier Madariaga <asier.madariaga@outlook.com>
2021-04-27 13:28:02 +0200

    Añadir e implementar interfaces IParqueNacionalRepository y ISenderoRepository.

---
+++ ParkyWeb/Repository/ParqueNacionalRepository.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class ParqueNacionalRepository : Repository<ParqueNacional>, IParqueNacionalRepository
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ParqueNacionalRepository(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
---
+++ ParkyWeb/Repository/SenderoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class SenderoRepository : Repository<Sendero>, ISenderoRepository
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public SenderoRepository(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
