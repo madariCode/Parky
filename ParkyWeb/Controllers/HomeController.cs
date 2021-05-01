625349e0052ebef312d49ffd39bb1cf94d291c87
Asier Madariaga <asier.madariaga@outlook.com>
2021-05-01 09:49:10 +0200

    Vista Home.

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParkyWeb.Models;
+using ParkyWeb.Models.ViewModels;
+using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,17 +14,27 @@ namespace ParkyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IParqueNacionalRepository _pnRepo;
+        private readonly ISenderoRepository _sRepo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IParqueNacionalRepository pnRepo, ISenderoRepository sRepo)
         {
             _logger = logger;
+            _pnRepo = pnRepo;
+            _sRepo = sRepo;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            IndexVM lista = new IndexVM()
+            {
+                ParqueNacionalLista = await _pnRepo.GetAllAsync(SD.ParquesNacionalesAPIPath),
+                SenderoLista = await _sRepo.GetAllAsync(SD.SenderosAPIPath)
+            };
 
+            return View(lista);
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ ParkyWeb/Models/ViewModels/IndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Models.ViewModels
+{
+    public class IndexVM
+    {
+        public IEnumerable<ParqueNacional> ParqueNacionalLista { get; set; }
+        public IEnumerable<Sendero> SenderoLista { get; set; }
+    }
+}
