2833804638c6cb20ba9451ace7edd7509733146b
Asier Madariaga <asier.madariaga@outlook.com>
2021-05-04 11:33:51 +0200

    Añadir UsuariosController. Método autentificar.

---
+++ ParkyAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Modelos;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/usuarios")]
+    [ApiController]
+    public class UsuariosController : Controller
+    {
+        private readonly IUsuarioRepository _uRepo;
+
+        public UsuariosController(IUsuarioRepository uRepo)
+        {
+            _uRepo = uRepo;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("autentificar")]
+        public IActionResult Autentificar([FromBody] Usuario modelo)
+        {
+            var usuario = _uRepo.Autentificar(modelo.Nombre, modelo.contraseña);
+            if(usuario == null)
+            {
+                return BadRequest(new { message = "El usuario o la contraseña son incorrectos!" });
+            }
+            return Ok(usuario);
+        }
+    }
+}
